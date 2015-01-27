143627f30fa387fac81cc98b0b38e986adf30ecf
= <humer015@umn.edu>
2015-01-26 18:29:04 -0600

    Start to Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def permission_denied
+      render :file => "public/401.html", :status => :unauthorized
+  end
 end

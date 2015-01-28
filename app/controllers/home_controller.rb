2dfc5f01209c1c3ee8101de205cf84a65e0b3bdf
Aaron Humerickhouse <aaron.humerickhouse@sportngin.com>
2015-01-28 13:58:45 -0600

    Basic layout and login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  before_action :current_user
+
   def show
+  	puts "Current User = #{@current_user}"
+  	if @current_user
+  		redirect_to(controller: 'boards', action: 'index')
+   	end
   end
 end

d5af7bcf644c8501f337218d5c2a59ab1617dc88
= <humer015@umn.edu>
2015-01-29 23:21:20 -0600

    removing debug outputs, fixing message form

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  puts "PRODUCTIOn? #{Rails.env.production?}"
   if Rails.env.production?
-    puts "In production"
     provider :google_oauth2, '575162953315-phn44fikq4tucmoncsa1nbgkgmi5u0g4.apps.googleusercontent.com', "#{ENV['OAUTH_SECRET']}", {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
   else
-    puts "Not in production"
-    puts "#{ENV['OAUTH_SECRET_LOCAL']}"
     provider :google_oauth2, '575162953315-ckd3bcfo5m1r32kmqlni8j2qejdl81i8.apps.googleusercontent.com', "#{ENV['OAUTH_SECRET_LOCAL']}", {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
   end
 end
